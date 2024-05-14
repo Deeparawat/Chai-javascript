1c9f2e13f647c09507402c2691e50ebf1f3f71da
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2024-05-14 04:39:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treate all js code as newer version js code
+// alert(3+3) does not work in node only works for browser
+// code readability should be high
+let name ="Deepa" // string
+let age =24
+// datatypes
+
+/* numver = 2 to power of 53
+bigint 
+boolean =>  true / false
+string = " "
+null -> of object type [standalone value]
+undefined
+symbol => unique 
+*/
+
+// Non primitve data
+
+console.log(typeof undefined);
+console.log(typeof null);
