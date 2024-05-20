04c6ed713e855a7c313e5c180434f4a5fa529b59
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2024-05-20 15:00:14 +0000

    "functions"

--- 03_basics/01functions.js
+++ 03_basics/01functions.js
@@ -29,3 +29,21 @@ function returnValue (getArray){
     console.log(getArray[1]) ;
 }
 returnValue(myNewArray)
+
+// 2 methods of writing functions 
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1 
+}
+
+// In this above syntax call before definition is working
+
+addTwo(5)
+
+const addTwo = function(num){
+    console.log(num+2); 
+}
+//In this case it is not working . there we have initialise here first and then used it.In
+
